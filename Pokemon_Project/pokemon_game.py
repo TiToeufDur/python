a70908df1ba993f3c25e4146d2200ce8a8677ef3
TiToeufDur <christophe.gravel.10@gmail.com>
2026-01-01 20:06:33 -0500

    ""

--- Pokemon_Project/pokemon_game.py
+++ Pokemon_Project/pokemon_game.py
@@ -2,12 +2,27 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Pokemon Battle")
+pygame.display.set_caption("Pokemon Game")
+pokemons = pygame.sprite.Group()
+
+class Pokemon(pygame.sprite.Sprite):
+    def __init__(self, name, ptype, hp, shield, attacks, image_path=None):
+        super().__init__()
+        self.name = name
+        self.type = ptype
+        self.hp = hp
+        self.shield = shield
+        self.attacks = attacks
 
 
 
-
-
+def create_bulbasaur():
+    attacks = {
+        "Tackle": {"damage": 10, "type": "Normal"},
+        "Vine Whip": {"damage": 15, "type": "Grass"},
+    }
+    bulbasaur = Pokemon("Bulbasaur", "Grass/Poison", 45, 5, attacks, "images/bulbasaur.png")
+    return bulbasaur
 
 
 
